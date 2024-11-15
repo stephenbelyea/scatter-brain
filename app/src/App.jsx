bb0a6c0978454fef153c02fd6afbaee19e1d82ad
Stephen Belyea <belyea.stephen@gmail.com>
2024-11-15 16:56:33 -0500

    Configure sanity client and initial persons request

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { client } from "./sanity/client";
+import "./App.css";
+
+const PERSONS_QUERY = `*[_type == "person"]`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    const fetchPersons = async () => {
+      const personsResponse = await client.fetch(PERSONS_QUERY);
+      setPersons(personsResponse);
+    };
+    if (persons.length === 0) {
+      fetchPersons();
+    }
+  }, [persons]);
 
   return (
     <>
+      <h1>Scatter Brain</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>People:</h2>
+        <ul>
+          {persons.map((person) => (
+            <li key={person.name}>{person.name}</li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
