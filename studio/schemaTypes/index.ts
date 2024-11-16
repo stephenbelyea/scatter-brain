82ee960e8937edfbcd1c85f849ef4c572a833a1c
Stephen Belyea <belyea.stephen@gmail.com>
2024-11-15 19:34:16 -0500

    Add listEntry schema

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,5 +1,6 @@
+import {listEntryType} from './listEntryType'
 import {personType} from './personType'
 import {taskItemType} from './taskItemType'
 import {taskListType} from './taskListType'
 
-export const schemaTypes = [personType, taskItemType, taskListType]
+export const schemaTypes = [personType, taskItemType, taskListType, listEntryType]
