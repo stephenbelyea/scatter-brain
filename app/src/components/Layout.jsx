34ae28d2c04b9c124d133696ef21b01c197f2340
Stephen Belyea <belyea.stephen@gmail.com>
2025-02-16 12:49:44 -0500

    Update layour

--- app/src/components/Layout.jsx
+++ app/src/components/Layout.jsx
@@ -6,17 +6,22 @@ import { Logo } from "./logo";
 
 import "./layout.css";
 import { ErrorState } from "./error-state";
+import { getSearchParams } from "../utils";
 
 export const Layout = ({ view = "", children }) => {
   const { persons, loading, error } = useContext(AppContext);
+  const { appendParams } = getSearchParams();
 
   return (
     <div className={`app layout view-${view}`}>
       <nav>
         <Logo />
-        <NavLink to="/">Home</NavLink>
+        <NavLink to={`/${appendParams}`}>Home</NavLink>
         {persons.map((person) => (
-          <NavLink key={person.slug} to={`/person/${person.slug}`}>
+          <NavLink
+            key={person.slug}
+            to={`/person/${person.slug}${appendParams}`}
+          >
             {person.name}
           </NavLink>
         ))}
