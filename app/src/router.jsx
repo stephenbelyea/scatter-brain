0845850136082232f73dd5c1f0602e22976d1424
Stephen Belyea <belyea.stephen@gmail.com>
2025-02-19 19:04:46 -0500

    Add conditional router basename

--- app/src/router.jsx
+++ app/src/router.jsx
@@ -1,17 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Dashboard, NotFound, Person } from "./views";
 
-export const appRouter = createBrowserRouter([
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-  {
-    path: "/",
-    element: <Dashboard />,
-  },
-  {
-    path: "person/:slug",
-    element: <Person />,
-  },
-]);
+const basename = import.meta.env.VITE_REACT_BASE_URL
+  ? import.meta.env.VITE_REACT_BASE_URL
+  : undefined;
+
+export const appRouter = createBrowserRouter(
+  [
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+    {
+      path: "/",
+      element: <Dashboard />,
+    },
+    {
+      path: "person/:slug",
+      element: <Person />,
+    },
+  ],
+  { basename }
+);
